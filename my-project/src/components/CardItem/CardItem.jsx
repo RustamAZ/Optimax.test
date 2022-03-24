ce6f1841ea1ce25422192aa6c72ebb082453f34e
RustamAZ <himynameisernie@yandex.ru>
2022-03-24 15:54:17 +0300

    Fix components/CardItem

--- my-project/src/components/CardItem/CardItem.jsx
+++ my-project/src/components/CardItem/CardItem.jsx
@@ -2,23 +2,22 @@ import { useState } from 'react';
 import classes from './CardItem.module.scss';
 import Popup from '../Popup/Popup';
 
-const CardItem = function(props) {
+const CardItem = function({dataItem, onAddedToCart}) {
     const [show, setShow] = useState(false);
 
     return (
         <>
             <li className={classes.card} onClick={() => {setShow(true)}}>
                 <picture>
-                    <img className={classes['card__img']} src={props.imgSrc} alt="" />
+                    <img className={classes['card__img']} src={dataItem.imgSrc} alt="" />
                 </picture>
 
-                <h2 className={classes['card__title']}>{props.name}</h2>
+                <h2 className={classes['card__title']}>{dataItem.name}</h2>
 
-                <span className={classes['card__price']}>Цена: {props.price}$</span>
+                <span className={classes['card__price']}>Цена: {dataItem.price}$</span>
             </li>
-            {show ? <Popup setActive={setShow} cardId={props.cardId} /> : null}
+            {show ? <Popup dataPopup={dataItem} setActive={setShow} onAddedToCart={onAddedToCart}/> : null}
         </>
-
     )
 };
 
