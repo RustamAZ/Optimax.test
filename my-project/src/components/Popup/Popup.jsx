5bddbcb2f98b2168c1371944c11285f8ee9c6470
RustamAZ <himynameisernie@yandex.ru>
2022-03-24 15:55:17 +0300

    Add popup for cardItem

--- my-project/src/components/Popup/Popup.jsx
+++ my-project/src/components/Popup/Popup.jsx
@@ -1,93 +1,46 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import classes from './Popup.module.scss';
-import { Loader } from '../Loader/Loader';
 import Button from '../Button/Button';
 
-const Popup = ({setActive, cardId}) => {
-    const [isLoadedPopup, setIsLoadedPopup] = useState(false);
-    const [error, setError] = useState(null);
-    const [popupData, setPopupData] = useState({});
+const Popup = ({setActive, dataPopup, onAddedToCart, id}) => {
     const [popupActive, setPopupActive] = useState(false);
 
-    const clickHandler = () => {
-        console.log(cardId);
-    }
-
-    useEffect(() => {
-        fetch(`http://localhost:3000/data/productData/${cardId}.json`)
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setIsLoadedPopup(true);
-                setPopupData(result);
-                setTimeout(() => {
-                    setPopupActive(true);
-                }, 100)
-            },
-            (error) => {
-                setIsLoadedPopup(true);
-                setError(error);
-            }
-        );
-    }, []);
-
-    if (error) {
-        return (
-            <div className={classes.popupContainer}>
-                <div onClick={() => {setActive(false)}} className={classes.popupOverlay}>
-                </div>
-                <div className={classes.popupBlock}> 
-                    Error...
-                </div>
-            </div>
-        )
-    } else if (!isLoadedPopup) {
-        return (
-            <div className={classes.popupContainer}>
-                <div onClick={() => {setActive(false)}} className={classes.popupOverlay}>
-                </div>
-                <Loader />
-            </div>
-        )
-    } else {
-        return (
+    return (
             <div className={classes.popupContainer}>
                 <div onClick={() => {setActive(false)}} className={classes.popupOverlay}>
                 </div>
 
                 <div className={popupActive ? classes.popupWrapper + ' ' + classes.active : classes.popupWrapper}>
                     <div className={classes.popupBlock}>
-                        <h1 className={classes.title}>{popupData.name}</h1>
+                        <h1 className={classes.title}>{dataPopup.name}</h1>
 
                         <div className={classes.wrapper}>
                             <div className={classes.imgWrapper}>
                                 <picture>
-                                    <img className={classes.cardImg} src={popupData.imgSrc} alt="" />
+                                    <img className={classes.cardImg} src={dataPopup.imgSrc} alt="" />
                                 </picture>
                             </div>
 
                             <div className={classes.cardInfoWrapper}>
                                 <ul className={classes.cardListInfo}>
-                                    <li className={classes.cardItemInfo}>Тип графического процессора: {popupData.type}</li>
-                                    <li className={classes.cardItemInfo}>Частота графического процессора: {popupData.gz} Мгц</li>
-                                    <li className={classes.cardItemInfo}>Объем видеопамяти: {popupData.ram} гб</li>
-                                    <li className={classes.cardItemInfo}>Количество CUDA ядер: {popupData.cuda}</li>
+                                    <li className={classes.cardItemInfo}>Тип графического процессора: {dataPopup.type}</li>
+                                    <li className={classes.cardItemInfo}>Частота графического процессора: {dataPopup.gz} Мгц</li>
+                                    <li className={classes.cardItemInfo}>Объем видеопамяти: {dataPopup.ram} гб</li>
+                                    <li className={classes.cardItemInfo}>Количество CUDA ядер: {dataPopup.cuda}</li>
                                 </ul>
 
                                 <div className={classes.cardPriceBlock}>
-                                    <span className={classes.cardPrice}> Актуальная цена: <span >{popupData.price}$</span></span>
-                                    <span className={classes.cardOldPrice}>Первичная цена: <span >{popupData.startedPrice}$</span></span>
+                                    <span className={classes.cardPrice}> Актуальная цена: <span >{dataPopup.price}$</span></span>
+                                    <span className={classes.cardOldPrice}>Первичная цена: <span >{dataPopup.startedPrice}$</span></span>
                                 </div>
 
-                                <Button clickHandler={clickHandler} type="button" text="Add to Cart"/>
+                                <Button clickHandler={() => {onAddedToCart(id)}} type="button" text="Add to Cart"/>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         )
-    }
- 
 }
 
 export default Popup;
